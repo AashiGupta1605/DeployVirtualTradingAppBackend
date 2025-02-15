15280788e38ae914ea7178ed95de0d249044a48b
Roshniparasharr <139575123+Roshniparasharr@users.noreply.github.com>
2025-02-15 11:06:40 +0530

    2

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,7 @@ export const registerUser = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({ name, email, password: hashedPassword });
+    console.log("HI");
 
     res.status(201).json({ message: "User registered successfully", user: newUser });
   } catch (error) {
