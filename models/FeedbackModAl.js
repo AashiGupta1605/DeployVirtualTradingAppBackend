27a9d68224280306147c14532e56802df31ce77e
Roshniparasharr <139575123+Roshniparasharr@users.noreply.github.com>
2025-02-27 13:20:56 +0530

    feedback and SubscriptionPlan modal

--- models/FeedbackModAl.js
+++ models/FeedbackModAl.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const FeedbackSchema = new mongoose.Schema({
+    userId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User", 
+        required: true 
+      },
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   feedbackCategory: {
