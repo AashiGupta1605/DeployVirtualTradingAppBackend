9b133f745e1776cf9c62d14d231f903c693a8d0c
Khushi Gupta <khushigupta3450@gmail.com>
2025-03-27 11:06:06 +0530

    forget password applied

--- routes/user/auth/authRoutes.js
+++ routes/user/auth/authRoutes.js
@@ -1,10 +1,12 @@
 // routes/user/authRoutes/authRoutes.js
 import express from 'express';
-import { registerUser, loginUser } from '../../../controllers/user/userController.js';
+import { registerUser, loginUser, forgotPassword, resetPassword } from '../../../controllers/user/userController.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
