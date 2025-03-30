acd72fc0a9cafaaac3847e3d17fa36907b1fa94b
AashiGupta1605 <115386205+AashiGupta1605@users.noreply.github.com>
2025-03-30 22:23:17 +0530

    add gallery in admin

--- routes/admin/adminRoute.js
+++ routes/admin/adminRoute.js
@@ -5,14 +5,13 @@ import nifty500DataRoutes from './niftyRoutes/nifty500dataRoutes.js';
 import organizationRoutes from './organizationRoutes/organizationRoutes.js';
 import userRoutes from './userRoutes/userRoutes.js';
 import etfRoutes from '../admin/etfRoutes/etfDataRoutes.js';
-<<<<<<< HEAD
+
 import galleryCategoryRoutes from '../admin/galleryRoutes/galleryCategoryRoutes.js'
 import galleryDataRoutes from '../admin/galleryRoutes/galleryDataRoutes.js'
-=======
+
 import eventRoutes from './eventRoutes/eventRoutes.js'; // Import the event routes
 
 import { getETFData } from '../../scripts/scraper2.js';
->>>>>>> 2865f2a15d4fa33ca1b8d79403a26d4f79552aa8
 
 const router = express.Router();
 
@@ -30,13 +29,12 @@ router.use('/', organizationRoutes);
 // User Routes
 router.use('/', userRoutes);
 
-<<<<<<< HEAD
+
 //Gallery Routes
 router.use('/galleryCategory', galleryCategoryRoutes)
 router.use('/gallery', galleryDataRoutes)
-=======
+
 // Event Routes
 router.use('/events', eventRoutes); // Add the event routes
->>>>>>> 2865f2a15d4fa33ca1b8d79403a26d4f79552aa8
 
 export default router;
